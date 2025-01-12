fb2ae9f311649a9823e20e1cf825ea2ccf7bd5fc
j0sep0z0 <j0sep0z0@outlook.com>
2025-01-12 17:43:31 +0100

    add email provider configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,16 +1,28 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "cloudinary",
+      provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
         upload: {},
         uploadStream: {},
         delete: {},
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: 'strapi-provider-email-resend',
+      providerOptions: {
+        apiKey: env('RESEND_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'hello@novawebinfinity.com',
+        defaultReplyTo: 'hello@novawebinfinity.com',
       },
     },
   },
